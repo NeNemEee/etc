2f1fff2d7c8b2a489e9c6a1354793de2eb464f58
NeNemEee <opinionq@gmail.com>
2022-03-16 11:53:25 +0900

    20220316

---
+++ cpp/MergeSort.cpp
@@ -0,0 +1,50 @@
+#ver.220315
+#배열을 넘겨주고 배열로 돌려받음
+ 
+int*mergeSort(int*_arr, int size){
+	if(size == 1){
+		return _arr;
+	}
+	int half = size/2;
+	
+	int*halfArr = new int[half];
+	for(int i=0; i<half; i++){
+		halfArr[i] = _arr[i];
+	}
+	
+	int*otherArr = new int[size-half];
+	for(int i=half; i<size; i++){
+		otherArr[i-half] = _arr[i];
+	}
+	
+	halfArr = mergeSort(halfArr, half);
+	otherArr = mergeSort(otherArr, (size - half));
+	
+	int i = 0;
+	int j = 0;
+	int k =0;
+	
+	while(k<size){
+		if(i<half&&j<(size-half)){
+			if(halfArr[i]<otherArr[j]){
+				_arr[k] = halfArr[i];
+				i++;
+			}else{
+				_arr[k] = otherArr[j];
+				j++;
+			}
+		}else if(i<half){
+			_arr[k]=halfArr[i];
+			i++;
+		}else if(j<size-half){
+			_arr[k]=otherArr[j];
+			j++;
+		}
+
+		
+		k++;
+	}
+	
+	return _arr;
+}
+
