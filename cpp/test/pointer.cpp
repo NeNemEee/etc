95db31f58e0115e48f0951e1da8b4936de6b906a
NeNemEee <opinionq@gmail.com>
2022-03-22 22:21:04 +0900

    20220322

--- cpp/test/pointer.cpp
+++ cpp/test/pointer.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int test(int**arr);
+
 int main(){
 	int**arr = new int*[5];
 	for(int i=0; i<5; i++){
@@ -9,9 +11,30 @@ int main(){
 		scanf("%d %d", &arr[i][0], &arr[i][1]);
 	}
 	
-	arr[1] = arr[0];
+	test(arr);
 	
-	cout<<arr[1][0]<<endl<<arr[1][1]<<endl;
+	for(int i=0; i<5; i++){
+		cout<<arr[i][0]<<" "<<arr[i][1]<<endl;
+	}
 	
 	return 0;
+}
+
+int test(int**arr){
+	int**tes = new int*[5];
+	for(int i=0; i<5; i++){
+		tes[i] = new int[2];
+		tes[i][0] = 1;
+		tes[i][1] = 1;
+
+	}
+	
+	for(int i=0; i<5; i++){
+		arr[i] = tes[i];
+		delete[] tes[i];
+	}
+	
+	delete[] tes;
+	
+	return 0;	
 }
